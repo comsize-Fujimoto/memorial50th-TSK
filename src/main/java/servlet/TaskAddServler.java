ca6b52aa3501f2cafcbbfeb62ab5661a0c35931d
comsize-saitomizuho <mizuho.saito@comsize.com>
2024-09-05 18:12:11 +0900

    Servlet完成

--- src/main/java/servlet/TaskAddServler.java
+++ src/main/java/servlet/TaskAddServler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -76,16 +75,14 @@ public class TaskAddServler extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 
-		//
 		//登録が完了した場合と失敗した場合で、遷移先を変更する。
 		//まずurlの初期値を空文字に設定する
 		String url = "";
-		
+
 		//DBで使うためのDate型のlimitDate変数を宣言（入れ物作り）
 		Date limitDate;
-		//limit_dateのデータが空でないかどうかを確認
-		/*limit_dateのデータが空でないときのif文
-		 * 期日(LimitDate)をint型に変換
+		//limit_dateのデータが空でないかどうかを確認するための条件分岐
+		 /* 期日(LimitDate)を型に変換
 		 * それ以外、期日(LimitDate)にnullが入る*/
 		if (!request.getParameter("LimitDate").isEmpty()) {
 			limitDate = Date.valueOf(request.getParameter("LimitDate"));
@@ -100,24 +97,18 @@ public class TaskAddServler extends HttpServlet {
 		// セッションスコープへユーザ名を設定
 		session.getAttribute("userName");
 		session.getAttribute("userId");
-		
-		
-		//？？？？
-		String userId = (String) session.getAttribute("userId");
-		String statusCode = request.getParameter("StatusCode");
-		String memo = request.getParameter("Memo");
-
-		//	
-		//
 
 		//登録内容をServletで受け取り、DAOに送る
 		//(タスク名、カテゴリ情報、期限)
 		String taskName = request.getParameter("TaskName");
 		int categoryId = Integer.parseInt(request.getParameter("CategoryId"));
 		String dateStr = request.getParameter("LimitDate");
+		String userId = (String) session.getAttribute("userId");
+		String statusCode = request.getParameter("StatusCode");
+		String memo = request.getParameter("memo");
 
-		/*初期値がnull(未設定)*/
-		LocalDate date = null;
+		
+		LocalDate localDate = LocalDate.parse(dateStr);
 
 		/*Date型のtoday変数＝DateクラスのvalueOf今の日付int型で取得	*/
 		Date today = Date.valueOf(LocalDate.now());
@@ -126,76 +117,46 @@ public class TaskAddServler extends HttpServlet {
 		 * 登録失敗画面へ遷移(エラーチェック)*/
 		/*trueの場合、urlを登録失敗画面へ遷移する*/
 		if (memo.length() > maxlength || (limitDate != null && limitDate.before(today))) {
-			url = "task-register-failure.jsp";
-		}else {
-			
-			/*String inputDataStr = request.getParameter("LimitDate");
-			SimpleDataFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			Date inputDate = sdf.parse("inputDate");
-			Date todday =new Date();*/
-			/*LocalDate date = null; 
-			
-			Date today = Date valueOf(LocalDate.now());
-			
-			if (memo.length() > maxlengs || (limitDate != null && limitDate.before(today))){
-				
-			}*/
+			url = "register-failure.jsp";
+		} else {
 
-			/*if(inputDate.before(today)) {
-				response. sendRedirect("register-failure.jsp");
-			}else {
-				
-			}*/
+			//バラバラなので↑を一塊の入れ物TaskBeanに入れる
+			//TaskBeanをインスタンス化
+			TaskBean task = new TaskBean();
 
-			//日付が入っていたらSQLに送る
-			if (dateStr != null) {
-				try {
-					date = LocalDate.parse(request.getParameter("LimitDate"));
-				} catch (DateTimeParseException e) {
-					date = null;
+			task.setTaskName(taskName);
+			task.setCategoryId(categoryId);
+			task.setLimitDate(localDate);
+			task.setUserId(userId);
+			task.setStatusCode(statusCode);
+			task.setMemo(memo);
+
+			//データの受け取りのためにインスタンス化
+			TaskCategoryDAO dao = new TaskCategoryDAO();
+
+			try {
+				/*daoクラスのinsertTaskメソッドに引数としてtaskを指定する。
+				 * メソッドの戻り値(登録件数)をcountに入れる。*/
+				int count = dao.insertTask(task);
+
+				/*登録件数が１の場合、登録完了画面
+				 * それ以外の場合、登録失敗画面*/
+				
+				if (count == 1) {
+					url = "register-success.jsp";
+				} else {
+					url = "register-failure.jsp";
 				}
+
+
+			} catch (SQLException | ClassNotFoundException e) {
+				e.printStackTrace();
+
 			}
 
+			
 		}
-
-		//バラバラなので↑を一塊の入れ物TaskBeanに入れる
-		//TaskBeanをインスタンス化
-		TaskBean task = new TaskBean();
-
-		task.setTaskName(taskName);
-		task.setCategoryId(categoryId);
-		task.setLimitDate(date);
-		task.setUserId(userId);
-		task.setStatusCode(statusCode);
-		task.setMemo(memo);
-
-		//データの受け取りのためにインスタンス化
-		TaskCategoryDAO dao = new TaskCategoryDAO();
-
-		try {
-			/*daoクラスのinsertTaskメソッドに引数としてtaskを指定する。
-			 * メソッドの戻り値(登録件数)をcountに入れる。*/
-			int count = dao.insertTask(task);
-
-			/*登録件数が１の場合、登録完了画面
-			 * それ以外の場合、登録失敗画面*/
-			RequestDispatcher rd;
-			if (count == 1) {
-				rd = request.getRequestDispatcher("register-success.jsp");
-			} else {
-				rd = request.getRequestDispatcher("register-failure.jsp");
-			}
-
-			/*転送する*/
-			rd.forward(request, response);
-
-		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-
-		}
-		
 		RequestDispatcher rd = request.getRequestDispatcher(url);
 		rd.forward(request, response);
 	}
-
 }
