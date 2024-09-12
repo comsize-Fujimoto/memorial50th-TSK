2e4b5dcc235dc10eedc79805e25b92a0de63e2f6
comsize-saitomizuho <mizuho.saito@comsize.com>
2024-09-12 15:41:36 +0900

    コメント微調整

--- src/main/java/model/dao/TaskCategoryDAO.java
+++ src/main/java/model/dao/TaskCategoryDAO.java
@@ -16,7 +16,7 @@ import model.entity.TaskBean;
 
 public class TaskCategoryDAO {
 
-	/* 全商品情報の一覧リストを返します。
+	/* 全商品情報の一覧リストを返す
 		 * @return 商品情報リスト
 		 * @throws SQLException
 		 * @throws ClassNotFoundException
@@ -26,18 +26,19 @@ public class TaskCategoryDAO {
 	 * 戻り値<CategoryBean>を扱うList型(オブジェクトクラス)のselectCategoryメソッド。
 	 * 引数なし
 	 * SQL、クラスが見つからない時の例外処理をselectCategoryメソッドの呼び出し元に投げる*/
-	public List<CategoryBean> selectCategory() 
+	public List<CategoryBean> selectCategory()
 			throws SQLException, ClassNotFoundException {
-		
-		//returnするためにCategoryBeanを扱うList型の変数名categoryListをインスタンス化する。
+
+		//戻り値を設定するためにCategoryBeanを扱うList型の変数名categoryListをインスタンス化
 		List<CategoryBean> categoryList = new ArrayList<CategoryBean>();
 		String sql = "SELECT * FROM m_category";
+
 		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
 		//中身を詰めていく
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);
-				
-				) {
+
+		) {
 			ResultSet res = pstmt.executeQuery();
 
 			// 結果の操作
@@ -50,18 +51,25 @@ public class TaskCategoryDAO {
 				category.setCategoryName(categoryName);
 				categoryList.add(category);
 			}
-		}return categoryList;
+		}
+		return categoryList;
 	}
-	public List<StatusBean> selectStatus() 
+
+	public List<StatusBean> selectStatus()
 			throws SQLException, ClassNotFoundException {
 
+		//戻り値を設定するためにCategoryBeanを扱うList型の変数名statusListをインスタンス化
+		//statusListにStatusBeanを格納
 		List<StatusBean> statusList = new ArrayList<StatusBean>();
+
 		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
 		try (Connection con = ConnectionManager.getConnection();
 				Statement stmt = con.createStatement();
 				ResultSet res = stmt.executeQuery("SELECT * FROM m_status")) {
 
 			// 結果の操作
+			//一行ずつデータを取り出して、すべての行を処理するためのループ
+			//行が存在する限り true を返す
 			while (res.next()) {
 				String statusCode = res.getString("status_code");
 				String statusName = res.getString("status_name");
@@ -70,46 +78,47 @@ public class TaskCategoryDAO {
 				status.setStatusName(statusName);
 				statusList.add(status);
 			}
-		}return statusList;
+		}
+		return statusList;
 	}
-//タスク登録メソッド
+
+	//タスク登録メソッド
 	public int insertTask(TaskBean newTask)
 			throws SQLException, ClassNotFoundException {
-		
+
 		//sql文の作成
 		String sql = "INSERT INTO t_task (task_name,category_id,limit_date,user_id,status_code,memo) VALUES (?,?,?,?,?,?);";
 
 		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
 		try (Connection con = ConnectionManager.getConnection();
 
-				//34行目抽出するために↓が必要
-				/*ConnectionオブジェクトのcreateStatementメソッドを使用する*/
+				/*SQL文を準備、実行*/
 				PreparedStatement pstmt = con.prepareStatement(sql)) {
 
-			//参照型StatementオブジェクトのexecuteQueryメソッドを使用
+			//値の設定
+			pstmt.setString(1, newTask.getTaskName());
+			pstmt.setInt(2, newTask.getCategoryId());
 			
-			pstmt.setString(1,newTask.getTaskName());
-			pstmt.setInt(2,newTask.getCategoryId());
 			Date limitDate = null;
-			if(newTask.getLimitDate()!= null){
-				try{
-					limitDate = Date.valueOf(newTask.getLimitDate()) ;
-				}catch(DateTimeParseException e){
+			if (newTask.getLimitDate() != null) {
+				try {
+					// データ型を文字列に変換
+					limitDate = Date.valueOf(newTask.getLimitDate());
+				} catch (DateTimeParseException e) {
 					limitDate = null;
-				}	
+				}
 			}
-			pstmt.setDate(3,limitDate);
-			pstmt.setString(4,newTask.getUserId());
-			pstmt.setString(5,newTask.getStatusCode());
-			pstmt.setString(6,newTask.getMemo());
-			
+			pstmt.setDate(3, limitDate);
+			pstmt.setString(4, newTask.getUserId());
+			pstmt.setString(5, newTask.getStatusCode());
+			pstmt.setString(6, newTask.getMemo());
+
 			//countに登録件数が代入される
 			int count = pstmt.executeUpdate();
-				
-				// 結果の操作
-				//resの間繰り返す
+
+			// 結果の操作
 			return count;
-}
 		}
+	}
 
 }
--- src/main/java/servlet/TaskAddServler.java
+++ src/main/java/servlet/TaskAddServler.java
@@ -31,13 +31,13 @@ public class TaskAddServler extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	//GETリクエストがあった場合
+	//GETリクエストがあった場合にdoGetメソッドを実行
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
 		try {
 			//リクエストのエンコーディングを指定
 			request.setCharacterEncoding("UTF-8");
-			
+
 			//TaskCategoryDAOに入っているメソッドを使うためにインスタンス化
 			TaskCategoryDAO dao = new TaskCategoryDAO();
 
@@ -50,18 +50,19 @@ public class TaskAddServler extends HttpServlet {
 			//カテゴリリストとステータスリストにselectメソッドの処理結果を代入
 			categoryList = dao.selectCategory();
 			statusList = dao.selectStatus();
-			
+
 			//session開始
 			HttpSession session = request.getSession();
-			//セッションにカテゴリーリストとステータスリストのデータを一時的に保存する
+
+			//セッションにカテゴリリストとステータスリストのデータを一時的に保存する
 			session.setAttribute("categoryList", categoryList);
 			session.setAttribute("statusList", statusList);
-			
+
 			//転送先の指定
 			RequestDispatcher rd = request.getRequestDispatcher("task-register.jsp");
 			rd.forward(request, response);
 		} catch (SQLException | ClassNotFoundException | ServletException | IOException e) {
-
+			//エラーの内容をコンソールに表示
 			e.printStackTrace();
 		}
 	}
@@ -77,12 +78,10 @@ public class TaskAddServler extends HttpServlet {
 		//まずurlの初期値を空文字に設定する
 		String url = "";
 
-		/*最大文字数制限を設定して条件分岐する*/
-		int maxlength = 100;
-
 		/*セッション呼び出し*/
 		HttpSession session = request.getSession();
-		// セッションスコープへユーザ名を設定
+		/*セッションスコープへユーザ名を設定
+		セッションにユーザーネームとユーザーIDのデータを一時的に保存する*/
 		session.getAttribute("userName");
 		session.getAttribute("userId");
 
@@ -96,27 +95,27 @@ public class TaskAddServler extends HttpServlet {
 
 		//DBで使うためのDate型のlimitDate変数を宣言（入れ物作り）
 		LocalDate limitDate;
-		
+
 		//limit_dateのデータが空でないかどうかを確認するための条件分岐
-		/* 期日(LimitDate)を型に変換
-		* それ以外、期日(LimitDate)にnullが入る*/
+		/* 期限(LimitDate)を型に変換
+		* それ以外、期限(LimitDate)にnullが入る*/
 		if (!request.getParameter("LimitDate").isEmpty()) {
 			limitDate = LocalDate.parse(request.getParameter("LimitDate"));
 		} else {
 			limitDate = null;
 		}
 
+		/*最大文字数制限を設定する*/
+		int maxlength = 100;
 		/*条件分岐　memoの文字数制限を超えている場合または、期日が過去の日付の場合、
 		 * 登録失敗画面へ遷移(エラーチェック)*/
 		/*trueの場合、urlを登録失敗画面へ遷移する*/
 		if (memo.length() > maxlength || (limitDate != null && limitDate.isBefore(LocalDate.now()))) {
 			url = "register-failure.jsp";
 		} else {
-
-			//バラバラなので↑を一塊の入れ物TaskBeanに入れる
 			//TaskBeanをインスタンス化
 			TaskBean task = new TaskBean();
-
+			//バラバラなので↓を一塊の入れ物TaskBeanに入れる
 			task.setTaskName(taskName);
 			task.setCategoryId(categoryId);
 			task.setLimitDate(limitDate);
